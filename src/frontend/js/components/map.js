70167bc1381071a3307a201517ede0cc67ac5bb4
Bryce Osterhaus <bryce.osterhaus@liferay.com>
2015-12-16 16:44:58 -0800

    Add transitions to expand each pin on map

--- src/frontend/js/components/map.js
+++ src/frontend/js/components/map.js
@@ -99,6 +99,27 @@ module.exports = React.createClass({
 		});
 	},
 
+	cycleUsers: function(svg) {
+		d3.selectAll('.map-user').each(
+			function(data, index) {
+				var delay = 5000;
+
+				d3.select(this)
+					.transition()
+					.delay(delay * index)
+						.duration(1000)
+						.attr('r', function(d) {
+							return 50;
+						})
+					.transition()
+					.delay((delay * index) + (delay - 1000))
+						.attr('r', function(d) {
+							return 2.5;
+						})
+			}
+		);
+	},
+
 	renderUserPins: function(svg, projection) {
 		var instance = this;
 
@@ -112,6 +133,8 @@ module.exports = React.createClass({
 
 					return 'translate(' + projection(arr) + ')';
 				});
+
+		instance.cycleUsers(svg);
 	},
 
 	render: function() {
